45026a3eaa6d18238b4a0ddbf86303a912e26cd7
Jason Kang <jasonkang14@gmail.com>
2024-01-07 14:21:42 +0900

    configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+            "primary":" #1d2745",
+            "secondary":" #1de5d4",
+            "tertiary":" #f52c50",
+            "white":" #ffffff",
+            "mono100":" #f1f1f1",
+            "mono200":" #bebebe",
+            "mono300":" #d6d7d9",
+            "error":" #d01e1e",
+        },
+      extend: {},
+    },
+    plugins: [],
+  }
