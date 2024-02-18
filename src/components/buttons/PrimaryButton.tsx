5add546d74e57ac145275527f643747bac86015c
Jason Kang <jasonkang14@gmail.com>
2024-02-18 15:58:16 +0900

    Fix disabled on primary button

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -11,6 +11,7 @@ const dark = "bg-primary text-white";
 const light = "bg-white text-primary";
 const social = "bg-social text-white";
 const modal = "text-white";
+const disabledStyle = "disabled:bg-mono100 disabled:text-mono200";
 
 const color: Record<PrimaryButtonTheme, string> = {
   dark,
@@ -27,7 +28,14 @@ export default function PrimaryButton({
 }: PrimaryButtonProps) {
   return (
     <button
-      className={`rounded-default disabled:bg-mono100 disabled:text-mono200 text-center text-base  w-full h-[59px] ${color[theme]}`}
+      className={`
+        rounded-default
+        text-center
+        text-base
+        w-full
+        h-[59px]
+        ${disabledStyle} 
+        ${color[theme]}`}
       disabled={disabled}
       type="button"
       onClick={onClick}
